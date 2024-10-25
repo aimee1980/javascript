3cecd004dcf91bd77b1cb0a9b3c2c4fc7c7fc469
Aimee <aimeelegrow@gmail.com>
2024-10-25 06:35:30 -0700

    .

---
+++ day04/script.js
@@ -0,0 +1,24 @@
+///alert (testing my js-file)
+
+
+///document.getElementById"mymessge"innerHTML="welcome to learning JAvescript"
+
+
+document.getElementById("my date").innerHTML=Date()
+
+/*
+
+function nameOfYourFunction()
+{
+}
+
+*/
+
+function nameOfYourFunction() 
+{
+
+}
+
+function MyGreeting()
+
+document.getElementById"mymessage
